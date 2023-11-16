d8ab10954b7ec87a25f797a6b5e6a1c38aba69f4
goyalk130 <goyalkaran130@gmail.com>
2023-11-17 00:04:51 +0530

    name chnaged logged user

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const nameSubmitButton = document.querySelector(".add-name-button")
-
+const loggedUser = document.querySelector(".logged-user")
 const nameinput = document.getElementById("name")
 
 nameSubmitButton.addEventListener("click",()=>{
     console.log(nameinput.value)
+    loggedUser.innerHTML = nameinput.value
 })
 
