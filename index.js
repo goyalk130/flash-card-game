1d654d5a7a9a79495cddd3820db07e884fb13480
goyalk130 <goyalkaran130@gmail.com>
2023-11-23 23:45:07 +0530

    .

--- index.js
+++ index.js
@@ -2,8 +2,16 @@ const nameSubmitButton = document.querySelector(".add-name-button")
 const loggedUser = document.querySelector(".logged-user")
 const nameinput = document.getElementById("name")
 
+const userNameInputtCon = document.querySelector(".user-name-input-con")
+
+
+
 nameSubmitButton.addEventListener("click",()=>{
     console.log(nameinput.value)
     loggedUser.innerHTML = nameinput.value
+    
+    console.log(userNameInputtCon.classList)
+    userNameInputtCon.classList.add("karan")
+    console.log(userNameInputtCon.classList)
 })
 
